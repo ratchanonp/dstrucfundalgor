e050f80dadbbc304b18f3473db0c54ea3077ea19
Ratchanon Panmas <qq.panmas@gmail.com>
2022-09-09 06:01:21 +0700

    :pencil: Add Comment

--- lab4/lab4.1.6434480323/src/datastr/Matrix.java
+++ lab4/lab4.1.6434480323/src/datastr/Matrix.java
@@ -1,12 +1,55 @@
 package datastr;
 
 public interface Matrix {
+    /**
+     * คืนค่าเป็นจำนวน row
+     * @return จำนวน row
+     */
     public int numRows();
 
+    /**
+     * คืนค่าเป็นจำนวน column
+     * @return จำนวน column
+     */
     public int numCols();
 
+    /**
+     * คืนค่า element ที่ row r , column c ในเมทริกซ์
+     * @param r row
+     * @param c column
+     * @return element ที่ row r , column c
+     */
     public double get(int r, int c);
+
+    /**
+     * แก้ค่า element ที่ ที่ row r , column c ในเมทริกซ์ เป็น v
+     * @param r row
+     * @param c column
+     * @param v ค่า
+     */
     public void set(int r, int c, double v);
+
+    /**
+     * คืน Matrix ที่เป็นผลบวกของ implicit parameter และ explicit parameter โดยจะ throws exception เมื่อไม่สามารถบวกเมทริกซ์ได้ (มิติของเมทริกซ์ไม่เท่ากัน)
+     * @param v explicit parameter
+     * @return Matrix ที่เป็นผลบวกของ implicit parameter และ explicit parameter
+     * @throws Exception เมื่อไม่สามารถบวกเมทริกซ์ได้ (มิติของเมทริกซ์ไม่เท่ากัน)
+     */
     public Matrix add(Matrix v) throws Exception;
-    public Matrix multiply(Matrix v) throws Exception;
+
+    /**
+     * ที่คืน Matrix ที่เป็นผลคูณของ implicit parameter และ explicit parameter โดยจะ throws exception เมื่อไม่สามารถคูณเมทริกซ์ได้ (incompatibility for multiplication)
+     * @param v explicit parameter
+     * @return Matrix ที่เป็นผลคูณของ Matrix implicit parameter และ Vector explicit parameter
+     * @throws Exception เมื่อไม่สามารถคูณเมทริกซ์ได้ (incompatibility for multiplication)
+     */
+    public Vector multiply(Vector v) throws Exception;
+
+    /**
+     * ที่คืน Matrix ที่เป็นผลคูณของ implicit parameter และ explicit parameter โดยจะ throws exception เมื่อไม่สามารถคูณเมทริกซ์ได้ (incompatibility for multiplication)
+     * @param m explicit parameter
+     * @return Matrix ที่เป็นผลคูณของ Matrix implicit parameter และ Matrix explicit parameter
+     * @throws Exception เมื่อไม่สามารถคูณเมทริกซ์ได้ (incompatibility for multiplication)
+     */
+    public Matrix multiply(Matrix m) throws Exception;
 }
--- lab4/lab4.1.6434480323/src/datastr/Vector.java
+++ lab4/lab4.1.6434480323/src/datastr/Vector.java
@@ -1,14 +1,71 @@
 package datastr;
 
 public interface Vector {
+    // ข้อ 1
+    /**
+     * ส่งมิติ (dimension) ของเวกเตอร์คืน
+     * @return มิติ (dimension) ของเวกเตอร์
+     */
     public int length();
+
+    /**
+     * ส่งขนาด (magnitude) ของเวกเตอร์คืน
+     * @return ขนาด (magnitude) ของเวกเตอร์
+     */
     public double magnitude();
+
+    /**
+     * คืนค่า element ที่ index ของ implicit parameter
+     * @param index ตำแหน่ง
+     * @return element ที่ index ของ implicit parameter
+     */
     public double get(int index);
+
+    /**
+     * แก้ค่า element ที่ index ของ implicit parameter เป็น value
+     * @param index ตำแหน่ง
+     * @param value ค่าใหม่
+     */
     public void set (int index, double value);
+
+    /**
+     * คืน Vector ที่เป็นผลบวกของ implicit parameter และ explicit parameter โดยจะ throws exception เมื่อไม่สามารถบวกเวกตอร์ได้ (มิติของเวกเตอร์ไม่เท่ากัน)
+     * @param v explicit parameter
+     * @return Vector ที่เป็นผลบวกของ implicit parameter และ explicit parameter
+     * @throws Exception เมื่อไม่สามารถบวกเวกตอร์ได้ (มิติของเวกเตอร์ไม่เท่ากัน)
+     */
     public Vector add(Vector v) throws Exception;
+
+    /**
+     * คืน Vector ที่เป็นผลลบของ implicit parameter และ explicit parameter โดยจะ throws exception เมื่อไม่สามารถลบเวกเตอร์ได้ (มิติของเวกเตอร์ไม่เท่ากัน)
+     * @param v explicit parameter
+     * @return ผลลบของ implicit parameter และ explicit parameter
+     * @throws Exception เมื่อไม่สามารถลบเวกเตอร์ได้ (มิติของเวกเตอร์ไม่เท่ากัน)
+     */
     public Vector subtract(Vector v) throws Exception;
-    public double dot(Vector V) throws Exception;
+
+
+    /**
+     * คืนจำนวนจริงที่เป็น dot product ของ implicit parameter และ explicit parameter โดยจะ throws exception เมื่อไม่สามารถหา dot product ได้ (มิติของเวกเตอร์ไม่เท่ากัน)
+     * @param v explicit parameter
+     * @return dot product ของ implicit parameter และ explicit parameter
+     * @throws Exception เมื่อไม่สามารถหา dot product ได้ (มิติของเวกเตอร์ไม่เท่ากัน)
+     */
+    public double dot(Vector v) throws Exception;
+
+    /**
+     * คืน Vector ที่เป็นผลคูณของ implicit parameter กับค่าคงที่ c
+     * @param c ค่าคงที่
+     * @return ผลคูณของ implicit parameter กับค่าคงที่ c
+     */
     public Vector multiply(double c);
 
-
+    // ข้อ 2
+    /**
+     * คืน Vector ที่เป็นผลคูณของ implicit parameter (Vector) ด้วย explicit parameter (Matrix) โดยจะ throws exception เมื่อไม่สามารถคูณได้ (incompatibility for multiplication)
+     * @param m explicit parameter (Matrix)
+     * @return ผลคูณของ implicit parameter (Vector) ด้วย explicit parameter (Matrix)
+     * @throws Exception เมื่อไม่สามารถคูณได้ (incompatibility for multiplication)
+     */
+    public Vector multiply(Matrix m) throws Exception;
 }
