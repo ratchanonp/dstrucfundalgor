8fb8e86e825c1b959f74e725a9cdddc62e7878e3
Ratchanon Panmas <qq.panmas@gmail.com>
2022-10-11 09:53:25 +0700

    :sparkles: Improve Readable and Comment

--- lab8/lab8.1.6434480323/src/datastr/BST.java
+++ lab8/lab8.1.6434480323/src/datastr/BST.java
@@ -21,33 +21,48 @@ public class BST {
         this.root = root;
     }
 
+    /**
+     * Method สำหรับเพิ่ม node ลงไปใน BST
+     * @param newElement ค่าที่จะเพิ่มลงไป
+     */
     public void add(int newElement) {
-        BTNode newNode = new BTNode(newElement, null, null);
-        if (root == null) root = newNode;
+        BTNode newNode = new BTNode(newElement, null, null);                      // สร้าง node ใหม่มีค่า newElement
+
+        // ตรวจสอบว่าต้นไม้นี้ว่างไหม
+        if (getRoot() == null) setRoot(newNode);                                            // ต้นไม้ว่างเพิ่มที่ root ได้เลย
         else {
-            BTNode r = root, node = null;
+            BTNode r = getRoot(), parent = null;                                            // r ใข้ในการ Traverse parent ใช้การจำ parent
+
+            // loop จนกว่าจะเจอ parent node ที่จะเอาไปเพิ่ม แล้วจำค่าในตัวแปร parent
             while (r != null) {
-                if (newElement < r.getElement()) {
-                    node = r;
-                    r = r.getLeft();
-                } else if (newElement > r.getElement()) {
-                    node = r;
-                    r = r.getRight();
-                } else return;
+                if      (newElement < r.getElement())   {parent = r; r = r.getLeft();}      // ค่ามากกว่า ไปทางซ้าย
+                else if (newElement > r.getElement())   {parent = r; r = r.getRight();}     // ค่าน้อยกว่า เดินไปทางขวา
+                else return;                                                                // กรณ๊ค่าเท่ากันแสดงว่ามีค่านั้นอยู่ใน Tree เรียบร้อยแล้ว
             }
 
-            if (newElement < node.getElement()) node.setLeft(newNode);
-            else node.setRight(newNode);
+            // ตรวจสอบว่าจะเพิ่ม node ใหม่ไปยังทางซ้ายหรือทางขวาของ parent node
+            if (newElement < parent.getElement()) parent.setLeft(newNode);                  // ถ้าค่าน้อยกว่า ให้เพิ่มไปทางซ้าย
+            else parent.setRight(newNode);                                                  // ถ้าค่ามากกว่า ให้เพิ่มไปทางขวา
         }
     }
 
+    /**
+     * Print ต้นไม้
+     * หากต้นไม้ว่างไม่มีข้อมูลที่ตำแหน่งรากให้ print "Tree is Empty"
+     * หากต้นไม้ไม่ว่างให้ print ค่าของต้นไม้โดย Traverse ไปในต้นไม้แบบ In-Order
+     */
     public void printTree() {
         BTNode r = getRoot();
         if (r == null) System.out.println("Tree is Empty");
         else inOrder(r);
+        System.out.println(); //ขึ้นบรรทัดใหม่
     }
 
-    public void inOrder(BTNode node) {
+    /**
+     * Recursive In-Order Tree Traversal
+     * @param node node ที่จะ print ค่า
+     */
+    private void inOrder(BTNode node) {
         if (node != null) {
             inOrder(node.getLeft());
             System.out.print(node.element + ",");
--- lab8/lab8.1.6434480323/src/lab/Main.java
+++ lab8/lab8.1.6434480323/src/lab/Main.java
@@ -5,15 +5,12 @@ import datastr.BST;
 public class Main {
     public static void main(String[] args) {
         BST tree = new BST();
-
         tree.add(3);
         tree.add(1);
         tree.add(2);
         tree.add(8);
         tree.add(15);
         tree.printTree();
-
-        System.out.println();
 
         BST tree2 = new BST();
         tree2.add(8);
